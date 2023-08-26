efe0657468b9532da7c4d4b36391803a691a9e2f
Rakesh Kusuma <103421485+kusumarakesh@users.noreply.github.com>
2023-08-26 13:00:03 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# flutter_ui_designing_starter
+ Flutter Training to Start with Designing UI
---
+++ android/app/src/main/kotlin/com/example/flutter_ui_designing_starter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_ui_designing_starter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
